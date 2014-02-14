c97a2c420f2661ce029f536ee2b5fbbc8a8dd64e
Yang <gnavvy@gmail.com>
2014-02-14 14:28:01 -0800

    some more refactoring

--- js/hexcoord.js
+++ js/hexcoord.js
@@ -20,7 +20,7 @@ App.ViewModels.HexMapViewModel = Backbone.Model.extend({
         if (!key) return this;   
 
         var attrs;
-        if (typeof(key) === 'object') {  // set multiple attributes
+        if (typeof key === 'object') {  // set multiple attributes
             attrs = key;
             options = val;
         } else {
@@ -35,11 +35,6 @@ App.ViewModels.HexMapViewModel = Backbone.Model.extend({
 
 // ------ VIEW ------ //
 App.Views.CanvasLayer = Backbone.View.extend({
-    initialize: function(options) {},
-    render: function() {
-        this.initCanvas();
-        return this;
-    },
     initCanvas: function() {
         var canvas = this.model.get('canvas');
         var margin = this.model.get('margin');
@@ -52,6 +47,7 @@ App.Views.CanvasLayer = Backbone.View.extend({
             .attr('height', canvas.height);
         this.layer.figure = this.layer.container.append('svg:g')
             .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        return this;
     },
     figureSize: function() {
         var canvas = this.model.get('canvas'),
@@ -63,55 +59,67 @@ App.Views.CanvasLayer = Backbone.View.extend({
     }
 });
 
-App.Views.CoordinateLayer = App.Views.CanvasLayer.extend({
-    initialize: function(options) {},
-    render: function() {
-        this.initCanvas();
-        this.initCoordinate();
+App.Views.CoordinateLayer = App.Views.CanvasLayer.extend({});
+
+App.Views.DataLayer = App.Views.CoordinateLayer.extend({
+    initData: function() {
+        this.hexagon();  // create a hexagon generator
+        this.data = {};
+        this.data.values = this.collection;
+        this.data.centroids = this.getCentroids();
         return this;
     },
-    initCoordinate: function() {
-        var gridDim = this.model.get('gridDim');
-        var figure = this.model.get('_figure');
+    draw: function() {
+        var hexbin = this.model.get('_hexagon');
 
-        var xRadius = figure.width / ((gridDim.x+0.5) * Math.sqrt(3));
-        var yRadius = figure.height / ((gridDim.y+1/3) * 1.5);
-        var hexRadius = d3.min([xRadius, yRadius]);
-        var hexbin = d3.hexbin().radius(hexRadius);
-
-        var centroids = [];
-        for (var y = 0; y < gridDim.y; ++y) {
-            for (var x = 0; x < gridDim.x; ++x) {
-                centroids.push([hexRadius*x*1.749, hexRadius*y*1.5]);
-            }
-        }      
-
-        var data = this.model.get('_data_series');
-        this.layer.coordinate = this.layer.figure.append("svg:g")
-            .selectAll('.hexagon')
-            .data(hexbin(centroids))
+        self = this;
+        this.layer.hexagons = this.layer.figure.append("svg:g").selectAll('.hexagon')
+            .data(hexbin(this.data.centroids))
             .enter().append("path")
             .attr("class", "hexagon")
             .attr("d", function(d) { return "M" + d.x + "," + d.y + hexbin.hexagon(); })
             .attr("stroke", function(d,i) { return "#AAA"; })
             .attr("stroke-width", "0.5px")
-            .style("fill", function(d,i) {
-                var x = i % gridDim.x;
-                var y = (i-x) / gridDim.x;
-                var opacity = data[y][x];
-                var hex = Number(parseInt(255 - 255 * opacity, 10)).toString(16);
-                return "#" + hex + hex + hex;
-            });
+            .style("fill", function(d,i) { return self.getColor(i); });
         return this;
     },
-    float2color: function(val) {
-        var dec = 255 * val;
-        var hex = Number(parseInt(dec, 10)).toString(16);
+    update: function() {
+        console.log('update!');
+        return this;
+    },
+    hexagon: function() {
+        var gridDim = this.model.get('gridDim');
+        var figureAear = this.model.get('_figure');
+        var xRadius = figureAear.width / ((gridDim.x+0.5) * Math.sqrt(3));
+        var yRadius = figureAear.height / ((gridDim.y+1/3) * 1.5);
+        var radius = d3.min([xRadius, yRadius]);
+        var hexagon = d3.hexbin().radius(radius);
+        this.model.set('_hexRadius', radius);
+        this.model.set('_hexagon', hexagon);
+        return this;
+    },
+    getCentroids: function() {
+        var radius = this.model.get('_hexRadius');
+        var gridDim = this.model.get('gridDim');
+
+        var centroids = [];  // faster than _.range().map().zip()
+        for (var y = 0; y < gridDim.y; ++y) {  
+            for (var x = 0; x < gridDim.x; ++x) {
+                centroids.push([radius * x * 1.749, radius * y * 1.5]);
+            }
+        }
+        return centroids;
+    },
+    getColor: function(idx) {
+        var gridDim = this.model.get('gridDim');
+        var x = idx % gridDim.x;
+        var y = (idx-x) / gridDim.x;
+        var opacity = 255 - 255 * this.data.values[y][x];
+        var hex = Number(parseInt(opacity, 10)).toString(16);
         return "#" + hex + hex + hex;
     }
 });
 
-App.Views.DataLayer = App.Views.CoordinateLayer.extend({});
 App.Views.AnnotationLayer = App.Views.DataLayer.extend({});
 App.Views.ListenerLayer = App.Views.AnnotationLayer.extend({});
 
@@ -121,13 +129,9 @@ App.Views.InteractionLayer = App.Views.ListenerLayer.extend({
     },
     initialize: function(options) {
         this.options = _.defaults(options || {}, this.options);
-        this.model.set('_data_series', this.collection);
     },
     render: function() {
-        this.initCanvas();
-        this.initCoordinate();
-        this.bindInteraction();
-        return this;
+        return this.initCanvas().initData().draw().bindInteraction();
     },
     bindInteraction: function() {
         var mouseover = function() {
@@ -140,7 +144,7 @@ App.Views.InteractionLayer = App.Views.ListenerLayer.extend({
                 .style("fill", function (d,i) { return "#EEE"; })
                 .style("fill-opacity", 1.0);
         }
-        this.layer.coordinate
+        this.layer.hexagons
             .on("mouseover", mouseover)
             .on("mouseout", mouseout);
         return this;
